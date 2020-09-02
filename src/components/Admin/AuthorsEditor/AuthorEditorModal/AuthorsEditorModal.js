dac092b14bc00a83bc172af626a6b8e446b28049
Dmitry Kudryavtsev <kudryavtsevdotcom@gmail.com>
2020-09-02 18:16:58 +0300

    AuthorsEditor.js edited to class component

--- src/components/Admin/AuthorsEditor/AuthorEditorModal/AuthorsEditorModal.js
+++ src/components/Admin/AuthorsEditor/AuthorEditorModal/AuthorsEditorModal.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Modal } from "@material-ui/core";
 import { Formik, Form } from "formik";
 import { CustomField } from "../../../../constants/CustomField";
 import { Button } from "@material-ui/core";
 import { useSelector, useDispatch } from "react-redux";
 import { addAuthor, editAuthor } from "../../../../redux/authors/actions";
+import { ModalWrapper } from "../../../../common/ModalWrapper";
 
 const AuthorEditorModal = ({
   modalFlag,
@@ -26,45 +26,39 @@ const AuthorEditorModal = ({
   };
 
   return (
-    <Modal
-      className="bookEditorModal-container"
-      open={modalFlag}
-      onClose={handleCloseModal}
-    >
-      <div className="bookEditorModal-paper">
-        <Formik
-          initialValues={
-            selectedAuthor
-              ? {
-                  fio: selectedAuthor.fio,
-                }
-              : {
-                  fio: "",
-                }
-          }
-          onSubmit={
-            selectedAuthor
-              ? (values) => editAuthors(values)
-              : (values) => sendAuthor(values)
-          }
-        >
-          {({ isSubmitting }) => (
-            <Form className="bookEditorModal-form">
-              <h3>Добавить автора</h3>
-              <CustomField required name="fio" label="Автор" type="text" />
-              <Button
-                disabled={isSubmitting}
-                type="submit"
-                variant="contained"
-                color="primary"
-              >
-                {selectedAuthor ? "Сохранить" : "Добавить"}
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </div>
-    </Modal>
+    <ModalWrapper open={modalFlag} onClose={handleCloseModal}>
+      <Formik
+        initialValues={
+          selectedAuthor
+            ? {
+                fio: selectedAuthor.fio,
+              }
+            : {
+                fio: "",
+              }
+        }
+        onSubmit={
+          selectedAuthor
+            ? (values) => editAuthors(values)
+            : (values) => sendAuthor(values)
+        }
+      >
+        {({ isSubmitting }) => (
+          <Form className="bookEditorModal-form">
+            <h3>Добавить автора</h3>
+            <CustomField required name="fio" label="Автор" type="text" />
+            <Button
+              disabled={isSubmitting}
+              type="submit"
+              variant="contained"
+              color="primary"
+            >
+              {selectedAuthor ? "Сохранить" : "Добавить"}
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </ModalWrapper>
   );
 };
 export default AuthorEditorModal;
